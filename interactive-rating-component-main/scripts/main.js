1038886bbcdf9d90be1a85ec850c17b3fcb7599d
alex <ologu.alexandru@gmail.com>
2022-04-26 16:02:02 +0300

    adding the ty part

--- interactive-rating-component-main/scripts/main.js
+++ interactive-rating-component-main/scripts/main.js
@@ -1,7 +1,23 @@
-const grades = document.querySelectorAll('.grade')
+const grades = document.querySelectorAll('.grade');
+const ratingInfo = document.querySelector('.ratingInfo');
+let myGrade = 0;
 
-for (const grade of grades) {
-    grade.addEventListener('click', () => {
-        grade.classList.toggle('selected');
+for (let grade_add of grades) {
+    grade_add.addEventListener('click', () => {
+        for (let grade_remove of grades) {
+            grade_remove.classList.remove('selected');
+        }
+        grade_add.classList.toggle('selected');
+        myGrade = grade_add.textContent;
     })
 }
+
+const submitBtn   = document.querySelector('.submit_btn');
+const first         = document.querySelector('.show')
+const second        = document.querySelector('.hide')
+
+submitBtn.addEventListener('click',() => {
+    first.classList.replace('show','hide');
+    second.classList.replace('hide','show');
+    ratingInfo.textContent = `You selected ${myGrade} out of 5`;
+});
