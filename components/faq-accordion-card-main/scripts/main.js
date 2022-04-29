20c7876f7ebcca3e74fa88210bb70c0292b2f6d4
alex <ologu.alexandru@gmail.com>
2022-04-29 13:43:24 +0300

    FAQ comp final

--- components/faq-accordion-card-main/scripts/main.js
+++ components/faq-accordion-card-main/scripts/main.js
@@ -1,15 +1,20 @@
 const questions = document.querySelectorAll('.question');
+const cube = document.querySelector('.cube');
 
 for (const question of questions) {
     question.addEventListener('click', () => {
         const divs = document.querySelectorAll('.question');
         for (const div of divs) {
-            div.classList.remove('bold');
+            div.classList.remove('selected');
             div.firstElementChild.classList.remove('rotateArrow');
             div.nextElementSibling.classList.remove('visibleAnswer');
         }
-        question.classList.add('bold');
-        question.firstElementChild.classList.toggle('rotateArrow');
-        question.nextElementSibling.classList.toggle('visibleAnswer');
+        if (!question.classList.contains('selected')) {
+            question.classList.add('selected');
+            question.firstElementChild.classList.toggle('rotateArrow');
+            question.nextElementSibling.classList.toggle('visibleAnswer');
+            // cube.classList.toggle('moveCube');
+        }
+
     })
 }
