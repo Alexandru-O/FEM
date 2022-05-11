61b1f990c160d1bb66083528c3d507c8edf7cb4c
alex <ologu.alexandru@gmail.com>
2022-05-11 13:24:32 +0300

    Time traking daskboard comp -- mobile done

--- components/time-tracking-dashboard-main/scripts/script.js
+++ components/time-tracking-dashboard-main/scripts/script.js
@@ -2,17 +2,32 @@ const daily = document.querySelector('.period span:nth-of-type(1)');
 const weekly = document.querySelector('.period span:nth-of-type(2)');
 const monthly = document.querySelector('.period span:nth-of-type(3)');
 
-async function getInfo() {
+const periods = document.querySelectorAll('.period span');
+
+const workCurrent = document.querySelectorAll('.blueContainer div:nth-of-type(3)');
+const workPrevious = document.querySelectorAll('.blueContainer div:nth-of-type(4)');
+
+async function getInfo(type) {
     await fetch('./data.json')
             .then( async response => response.json() )
-            .then( json => displayInfo(json) )
+            .then( json => displayInfo(json,type) )
             .catch(error => console.error(`Error fetching data: ${error.message}`) );
 }
 
-function displayInfo (obj) {
-    console.log(obj[0].title);
+function displayInfo (obj,type) {
+    for ( let i = 0 ; i < 6 ; i++ ) {
+        workCurrent[i].textContent = obj[i].timeframes[type].current + 'hrs';
+        workPrevious[i].textContent = `Previous - ${obj[i].timeframes[type].previous  + 'hrs'}`;
+    }
 }
 
-getInfo();
+for ( const period of periods ) {
+    period.addEventListener('click', (e) => {
+        periods.forEach( period => period.classList.remove('active'));
+        e.target.classList.add('active');
+        getInfo(e.target.textContent.toLowerCase());
+    });
+}
+
 
 
