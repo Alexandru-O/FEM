2f1f9c48fee3919f2434051bd2b0dca1a254941f
alex <ologu.alexandru@gmail.com>
2022-06-29 09:19:13 +0300

    Dashbord comp - done

--- components/social-media-dashboard-with-theme-switcher-master/scripts/script.js
+++ components/social-media-dashboard-with-theme-switcher-master/scripts/script.js
@@ -1,3 +1,30 @@
-const btn = document.querySelector('.toggle');
-const prefersDarkScheme = window.matchMedia('prefers-clolor-dark: dark');
+const btn = document.querySelector('.btn-toggle');
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
+if(prefersDarkScheme.matches)
+    btn.checked = true;
+else
+    btn.checked = false;
+
+//get user's theme pref
+const currentTheme = localStorage.getItem('theme');
+
+if(currentTheme == "dark") {
+    document.body.classList.toggle("dark-mode");
+} else if ("currentTheme" == "light") {
+    document.body.classList.toggle("light-mode");
+}
+
+//listen for click on change theme btn
+btn.addEventListener("click", () => {
+    if(prefersDarkScheme.matches) {
+        console.log("e aici 1");
+        document.body.classList.toggle("light-mode");
+        var theme = document.body.classList.contains("light-mode") ? "light" : "dark";
+    } else {
+        console.log("e aici 2");
+        document.body.classList.toggle("dark-mode");
+        var theme = document.body.classList.contains("dark-mode") ? "dark" : "light";
+    }
+    localStorage.setItem("theme", theme);
+})
