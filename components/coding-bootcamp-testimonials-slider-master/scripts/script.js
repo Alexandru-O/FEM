5780f9a214a91cb9442d8e049901bdf427c99f51
Alexandru-O Home_Desktop <ologu.alexandru@gmail.com>
2022-07-04 08:58:04 +0300

    fixing testimonial

--- components/coding-bootcamp-testimonials-slider-master/scripts/script.js
+++ components/coding-bootcamp-testimonials-slider-master/scripts/script.js
@@ -3,14 +3,34 @@ const next = document.querySelector('.arrow__next');
 
 const testimonial = document.querySelectorAll('.testimonial');
 
-for( var i = 0 ; i < testimonial.length ; i++) {
+// foreach testimonial we set a listener and check if we click on prev or next
+for( let i = 0 ; i <= testimonial.length-1 ; i++) {
     testimonial[i].addEventListener('click', (evt) => {
+        let n = i;
+        //if we press on PREV arrow
         if ( evt.target.classList.contains('arrows__prev') ) {
-            console.log(testimonial[i].classList);
-            // this.classList.remove('active');
-            // testimonial[i-1].classList.add('active');
+            //check if item is the most left one on the list
+            //if so, then we should slide to the last item on the list
+            if ( i == 0 )
+                n = testimonial.length-1;
+            else
+                n = i - 1;   
+            slide(n,i);
+        //if we press NEXT arrow
         } else if ( evt.target.classList.contains('arrows__next') ) {
-            console.log('arrow next');
+            //check if item is the last on the list
+            //if so, then we should slide to the first item on the list
+            if ( i == testimonial.length-1)
+                n = 0;
+            else
+                n = i + 1;
+            slide(n,i);
         }
     });
+}
+
+//add or remove ACTIVE class
+function slide(n,i) {
+    testimonial[n].classList.add('active');
+    testimonial[i] .classList.remove('active'); 
 }
